495204d57d4d348cfb8e1cb7ae7ee3789651915d
Dmitry Pimushkin <d.pimushkin@gmail.com>
2020-08-02 23:12:41 +0400

    Add setup method for unit test

--- GameStore.UnitTest/UnitTest1.cs
+++ GameStore.UnitTest/UnitTest1.cs
@@ -16,17 +16,25 @@ namespace GameStore.UnitTest
 {
     public class Tests
     {
+        private HomeController _homeController;
+
+        private static Mock<DbSet<T>> CreateDbSetMock<T>(IEnumerable<T> elements) where T : class
+        {
+            var elementsAsQueryable = elements.AsQueryable();
+            var dbSetMock = new Mock<DbSet<T>>();
+
+            dbSetMock.As<IQueryable<T>>().Setup(m => m.Provider).Returns(elementsAsQueryable.Provider);
+            dbSetMock.As<IQueryable<T>>().Setup(m => m.Expression).Returns(elementsAsQueryable.Expression);
+            dbSetMock.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(elementsAsQueryable.ElementType);
+            dbSetMock.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(elementsAsQueryable.GetEnumerator());
+
+            return dbSetMock;
+        }
+
         [SetUp]
         public void Setup()
         {
-        }
-
-        [Test]
-        public void Can_Paginate()
-        {
-            // Arrange
             const int pageSize = 3;
-            const int page = 2;
             var games = new List<Game>
             {
                 new Game {Id = 1, Name = "Game1"},
@@ -41,29 +49,20 @@ namespace GameStore.UnitTest
             applicationDbContextMock.Setup(x => x.Set<Game>()).Returns(gamesMock.Object);
             var unitOfWork = new UnitOfWork(applicationDbContextMock.Object);
             var gameService = new GameService(unitOfWork);
-            var homeController = new HomeController(null, gameService) {PageSize = pageSize};
+            _homeController = new HomeController(null, gameService) {PageSize = pageSize};
+        }
 
+        [Test]
+        public void Can_Paginate()
+        {
             // Act
-            var result = (GamesListViewModel) ((ViewResult) homeController.Index(page)).Model;
+            var result = (GamesListViewModel) ((ViewResult) _homeController.Index(2)).Model;
 
             // Assert
             var resultGames = result.Games.ToList();
             Assert.IsTrue(resultGames.Count == 2);
             Assert.AreEqual(resultGames[0].Name, "Game4");
             Assert.AreEqual(resultGames[1].Name, "Game5");
-        }
-
-        private static Mock<DbSet<T>> CreateDbSetMock<T>(IEnumerable<T> elements) where T : class
-        {
-            var elementsAsQueryable = elements.AsQueryable();
-            var dbSetMock = new Mock<DbSet<T>>();
-
-            dbSetMock.As<IQueryable<T>>().Setup(m => m.Provider).Returns(elementsAsQueryable.Provider);
-            dbSetMock.As<IQueryable<T>>().Setup(m => m.Expression).Returns(elementsAsQueryable.Expression);
-            dbSetMock.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(elementsAsQueryable.ElementType);
-            dbSetMock.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(elementsAsQueryable.GetEnumerator());
-
-            return dbSetMock;
         }
 
         [Test]
@@ -95,27 +94,8 @@ namespace GameStore.UnitTest
         [Test]
         public void Can_Send_Pagination_View_Model()
         {
-            // Arrange
-            const int pageSize = 3;
-            const int page = 2;
-            var games = new List<Game>
-            {
-                new Game {Id = 1, Name = "Game1"},
-                new Game {Id = 2, Name = "Game2"},
-                new Game {Id = 3, Name = "Game3"},
-                new Game {Id = 4, Name = "Game4"},
-                new Game {Id = 5, Name = "Game5"}
-            }.AsQueryable();
-            var gamesMock = CreateDbSetMock(games);
-            var options = new DbContextOptions<ApplicationDbContext>();
-            var applicationDbContextMock = new Mock<ApplicationDbContext>(options);
-            applicationDbContextMock.Setup(x => x.Set<Game>()).Returns(gamesMock.Object);
-            var unitOfWork = new UnitOfWork(applicationDbContextMock.Object);
-            var gameService = new GameService(unitOfWork);
-            var homeController = new HomeController(null, gameService) { PageSize = pageSize };
-
             // Act
-            var result = (GamesListViewModel)((ViewResult)homeController.Index(page)).Model;
+            var result = (GamesListViewModel) ((ViewResult) _homeController.Index(2)).Model;
 
             // Assert
             var pageInfo = result.PagingInfo;
